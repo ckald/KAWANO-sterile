51f1bde91254a383a67ca680e63de6e0ce715125
Andrii Magalich <andrew.magalich@gmail.com>
2015-05-07 18:36:50 +0200

    Fix binary_search and add tests

--- interpolation.f90
+++ interpolation.f90
@@ -3,32 +3,48 @@
 !-------------TO FIND THE INTERPOLATION INDEX----------------
 
 SUBROUTINE binary_search(head, tail, needle, haystack, size, decreasing)
+  ! Find the insert position for the `needle` that preserves the ordering of the
+  ! `haystack`. Comparing to the regular binary search, this subroutine returns
+  ! a pair of indices indicating the insertion interval. If the `needle` is
+  ! found in the `haystack`, those indices coincide.
+
 
   IMPLICIT NONE
 
   INTEGER, INTENT(out) :: head
   INTEGER, INTENT(out) :: tail
-  REAL, INTENT(in) :: needle
+  DOUBLE PRECISION, INTENT(in) :: needle
   INTEGER :: size
   !f2py INTEGER, INTENT(hide), DEPEND(haystack) :: size = len(haystack)
-  REAL, INTENT(in), DIMENSION(size) :: haystack
+  DOUBLE PRECISION, INTENT(in), DIMENSION(size) :: haystack
   LOGICAL, INTENT(in) :: decreasing
+  !f2py LOGICAL, INTENT(in), OPTIONAL :: decreasing = 0
 
   INTEGER range
   INTEGER middle
 
-  REAL comparison
-
-  head = 1
-  tail = size
-  range = tail - head
-  middle = (tail + head) / 2
+  DOUBLE PRECISION comparison
 
   IF (decreasing) THEN
     comparison = -1.
   ELSE
     comparison = 1.
   END IF
+
+  head = 1
+  tail = size
+
+  ! Check if the `needle` is outside the `haystack` range
+  IF (SIGN(1., needle - haystack(head)) == -comparison) THEN
+    tail = head
+    RETURN
+  ELSE IF (SIGN(1., needle - haystack(tail)) == comparison) THEN
+    head = tail
+    RETURN
+  END IF
+
+  range = tail - head
+  middle = (tail + head) / 2
 
   DO WHILE ( haystack(middle) /= needle .AND. range > 1)
     IF (SIGN(1., needle - haystack(middle)) == comparison) THEN
@@ -41,6 +57,7 @@ SUBROUTINE binary_search(head, tail, needle, haystack, size, decreasing)
     middle = (tail + head) / 2
   END DO
 
+  ! Identify if the needle is positioned exactly at `middle`/`head`/`tail`
   IF (haystack(middle) == needle) THEN
     head = middle
     tail = middle
@@ -55,29 +72,30 @@ END SUBROUTINE binary_search
 
 !-------THE FUNCTIONS DOING AN INTERPOLATION---------------
 
-SUBROUTINE interp_values(interp_val, x_interp, x_values, y_values, decreasing, nlines)
+SUBROUTINE interp_values(interp_val, x_interp, x_values, y_values, decreasing, size)
 
   IMPLICIT NONE
 
-  REAL, INTENT(out) :: interp_val
+  DOUBLE PRECISION, INTENT(out) :: interp_val
   !The interpolated y-result
-  REAL, INTENT(in) :: x_interp
+  DOUBLE PRECISION, INTENT(in) :: x_interp
   !The x-value at which interpolation happens
-  REAL, INTENT(in) :: x_values(nlines)
+  DOUBLE PRECISION, INTENT(in) :: x_values(size)
   !The abscissa discrete values
-  REAL, INTENT(in) :: y_values(nlines)
+  DOUBLE PRECISION, INTENT(in) :: y_values(size)
   !The array for which we interpolate
   LOGICAL, INTENT(in) :: decreasing
-  !This boolean tells if the x-values are in decreasing (true) or incr. order
-  INTEGER, INTENT(in) :: nlines
+  !f2py LOGICAL, INTENT(in), OPTIONAL :: decreasing = 0
+  INTEGER :: size
+  !f2py INTEGER, INTENT(hide), DEPEND(x_values) :: size = len(x_values)
 
 
   INTEGER head, tail
   !The integer giving an x-value just below the one to be interpolated
 
-  REAL :: x_lo, x_up, y_lo, y_up
+  DOUBLE PRECISION :: x_lo, x_up, y_lo, y_up
 
-  CALL binary_search(head, tail, x_interp, x_values, nlines, decreasing)
+  CALL binary_search(head, tail, x_interp, x_values, size, decreasing)
 
   IF (head /= tail) THEN
 
@@ -93,34 +111,35 @@ SUBROUTINE interp_values(interp_val, x_interp, x_values, y_values, decreasing, n
 
 END SUBROUTINE
 
-!--------FOR A LOGARITHMIC INTERPOLATION---------------------
+!--------LOGARITHMIC INTERPOLATION---------------------
 ! We will usually do an interpolation of the log of the values as these
 !  values are rapidly varying, instead of simply doing a linear interp.
-SUBROUTINE log_interp_values(interp_val, x_interp, x_values, y_values, decreasing, nlines)
+SUBROUTINE log_interp_values(interp_val, x_interp, x_values, y_values, decreasing, size)
 
   IMPLICIT NONE
 
-  REAL, INTENT(out) :: interp_val
+  DOUBLE PRECISION, INTENT(out) :: interp_val
   !The interpolated y-result
-  REAL, INTENT(in) :: x_interp
+  DOUBLE PRECISION, INTENT(in) :: x_interp
   !The x-value at which interpolation happens
-  REAL, INTENT(in), DIMENSION(nlines) :: x_values
+  DOUBLE PRECISION, INTENT(in), DIMENSION(size) :: x_values
   !The abscissa
-  REAL, INTENT(in), DIMENSION(nlines) :: y_values
+  DOUBLE PRECISION, INTENT(in), DIMENSION(size) :: y_values
   !The array for which we interpolate
   LOGICAL, INTENT(in) :: decreasing
-  !This boolean tells if the x-values are in decreasing (true) or incr. order
-  INTEGER, INTENT(in) :: nlines
+  !f2py LOGICAL, INTENT(in), OPTIONAL :: decreasing = 0
+  INTEGER :: size
+  !f2py INTEGER, INTENT(hide), DEPEND(x_values) :: size = len(x_values)
 
   INTEGER head, tail
   !The integer giving an x-value just below the one to be interpolated
   LOGICAL positive
   !This tells if the y_values are positive, needed when taking the log.
 
-  REAL x_lo, x_up, y_lo, y_up
+  DOUBLE PRECISION x_lo, x_up, y_lo, y_up
 
 
-  CALL binary_search(head, tail, x_interp, x_values, nlines, decreasing)
+  CALL binary_search(head, tail, x_interp, x_values, size, decreasing)
 
   IF (head == tail) THEN
     interp_val = y_values(head)
@@ -131,7 +150,7 @@ SUBROUTINE log_interp_values(interp_val, x_interp, x_values, y_values, decreasin
     IF ( y_lo*y_up <= 0 ) THEN
     ! It is not possible to take the log if one of them is 0 or if they
     ! have different signs, so we do a linear interpolation in this case
-       CALL interp_values(interp_val, x_interp, x_values, y_values, decreasing, nlines)
+       CALL interp_values(interp_val, x_interp, x_values, y_values, decreasing, size)
     ELSE
        positive = .true.
        IF (y_lo < 0) THEN
@@ -148,6 +167,73 @@ SUBROUTINE log_interp_values(interp_val, x_interp, x_values, y_values, decreasin
        interp_val = ( (x_up-x_interp)*y_lo + (x_interp-x_lo)*y_up ) / (x_up - x_lo)
 
        interp_val = exp(interp_val)
+
+       IF (.not.positive) THEN
+          interp_val = -interp_val
+       END IF
+
+    END IF
+
+  END IF
+
+END SUBROUTINE
+
+!--------DISTRIBUTION FUNCTION LOGARITHMIC INTERPOLATION---------------------
+! We will usually do an interpolation of the log of the values as these
+!  values are rapidly varying, instead of simply doing a linear interp.
+SUBROUTINE dist_interp_values(interp_val, x_interp, x_values, y_values, decreasing, size)
+
+  IMPLICIT NONE
+
+  DOUBLE PRECISION, INTENT(out) :: interp_val
+  !The interpolated y-result
+  DOUBLE PRECISION, INTENT(in) :: x_interp
+  !The x-value at which interpolation happens
+  DOUBLE PRECISION, INTENT(in), DIMENSION(size) :: x_values
+  !The abscissa
+  DOUBLE PRECISION, INTENT(in), DIMENSION(size) :: y_values
+  !The array for which we interpolate
+  LOGICAL, INTENT(in) :: decreasing
+  !f2py LOGICAL, INTENT(in), OPTIONAL :: decreasing = 0
+  INTEGER :: size
+  !f2py INTEGER, INTENT(hide), DEPEND(x_values) :: size = len(x_values)
+
+  INTEGER head, tail
+  !The integer giving an x-value just below the one to be interpolated
+  LOGICAL positive
+  !This tells if the y_values are positive, needed when taking the log.
+
+  DOUBLE PRECISION x_lo, x_up, y_lo, y_up
+
+
+  CALL binary_search(head, tail, x_interp, x_values, size, decreasing)
+
+  IF (head == tail) THEN
+    interp_val = y_values(head)
+  ELSE
+    y_lo = y_values(head)
+    y_up = y_values(tail)
+
+    IF ( y_lo*y_up <= 0 ) THEN
+    ! It is not possible to take the log if one of them is 0 or if they
+    ! have different signs, so we do a linear interpolation in this case
+       CALL interp_values(interp_val, x_interp, x_values, y_values, decreasing, size)
+    ELSE
+       positive = .true.
+       IF (y_lo < 0) THEN
+          positive = .false.
+          y_lo = -y_lo        !For negative numbers we take the log
+          y_up = -y_up        ! of their opposite
+       END IF
+
+       y_lo = log(1 / y_lo - 1)
+       y_up = log(1 / y_up - 1)
+       x_lo = x_values(head)
+       x_up = x_values(tail)
+
+       interp_val = ( (x_up-x_interp)*y_lo + (x_interp-x_lo)*y_up ) / (x_up - x_lo)
+
+       interp_val = 1 / (exp(interp_val) + 1)
 
        IF (.not.positive) THEN
           interp_val = -interp_val
